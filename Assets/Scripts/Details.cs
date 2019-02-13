3fd05ef7724bf3ded86b4088cc1d5a13815ff073
alexisbdr <alexis.baudron15@gmail.com>
2019-02-13 15:35:22 -0600

    safety push before transfer

--- Assets/Scripts/Details.cs
+++ Assets/Scripts/Details.cs
@@ -13,30 +13,8 @@ public class Details : MonoBehaviour {
     //player money
     double money;
 
-    //planet data
-    int NumPlanets;
-    double[] Populations;
-    double[] TaxRates;
-    double[] PopCapacities;
-    double[] PopIncreaseCosts;
-    string[] PlanetNames;
-
     //which planet was clicked most recently
     int ActivePlanetId;
-
-    //modifier for pop gain when population increase button is added
-    double PopIncreaseModifier = 100000;
-    //money cost for pop gain when population increase button is pushed
-    double PopIncreaseCostBase = 1000;
-    //threshold for pop base necessary to press gain pop button
-    double PopIncreaseThreshold = 100000;
-    //modifier for pop growth on updates
-    double PopGrowthRate = 0.0003;
-    //money cost for colonization of new planet
-    double ColonizeMoneyCost = 10000;
-    //pop cost for colonization of new planet
-    double ColonizePopCost = 10000;
-
 
 	// Use this for initialization
 	void Start () {
@@ -91,46 +69,6 @@ public class Details : MonoBehaviour {
             //Model 2: growth towards capacity - may be negative
             Populations[i] += PopGrowthRate * Populations[i]*(Math.Log10(Math.Max(1,PopCapacities[i])) - Math.Log10(Math.Max(1, Populations[i]))) * Time.fixedDeltaTime;
         }
-    }
-
-    public void InitializePlanetDetails(int startingPlanet, int nP, long startingPopulation)
-    {
-        NumPlanets = nP;
-        ActivePlanetId = startingPlanet;
-        Populations = new double[NumPlanets];
-        TaxRates = new double[NumPlanets];
-        PopCapacities = new double[NumPlanets];
-        PopIncreaseCosts = new double[NumPlanets];
-        PlanetNames = new string[NumPlanets];
-        for (int i = 0; i < NumPlanets; i++)
-        {
-            Populations[i] = 0;
-            PopCapacities[i] = 1000000000;
-            TaxRates[i] = 0;
-            PopIncreaseCosts[i] = PopIncreaseCostBase;
-            PlanetNames[i] = GeneratePlanetName();
-            if (i == startingPlanet)
-            {
-                TaxRates[i] = 0.0001;
-                Populations[i] = startingPopulation;
-                PopCapacities[i] = 6000000000;
-                PlanetNames[i] = "Gaia";
-            }
-        }
-    }
-
-    public string GeneratePlanetName()
-    {
-        string[] possibleStartingConsonants = {"B", "C", "D", "F","G","H","J","K","L","M","N",
-            "P","Q","R","S","T","V","X","Z", "Ch", "St", "Tl","Ts","Sh","Dh","Ph","Kr",
-            "Pl","Pr","Th","Zh","'" };
-        string[] possibleVowels = { "a", "i", "o", "u", "e","a","i","o","u","e","a","i","o","u","e",
-            "ai", "ou", "uo", "oi", "ae", "ao", "ei", "ua", "oe", "ui", "ue", "ia", "io", "iu", "ie" };
-        string[] possibleMiddleConsonants = { "b","c","d","f","g","j","k","l","m","n","p","q","r","s","t","v","z"};
-        return possibleStartingConsonants[(int)Math.Floor((double)UnityEngine.Random.Range(0, possibleStartingConsonants.Length - 0.1f))] +
-            possibleVowels[(int)Math.Floor((double)UnityEngine.Random.Range(0, possibleVowels.Length - 0.1f))] +
-            possibleMiddleConsonants[(int)Math.Floor((double)UnityEngine.Random.Range(0, possibleMiddleConsonants.Length - 0.1f))] +
-            possibleVowels[(int)Math.Floor((double)UnityEngine.Random.Range(0, possibleVowels.Length - 0.1f))];
     }
     
     public void AddPopulation()
