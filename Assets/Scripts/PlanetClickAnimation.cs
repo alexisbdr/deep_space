296de8a30598effefae2460bdcc26ac3c819b79d
Abizar <babyzar97@gmail.com>
2019-02-18 12:52:30 -0600

    moved animation to script instead of animator controller

--- Assets/Scripts/PlanetClickAnimation.cs
+++ Assets/Scripts/PlanetClickAnimation.cs
@@ -2,30 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// Controls color of animation, destroys game object when animation finished. (Look at AnimatorController component for movement animation.)
+// Controls color and y pos movement animation, destroys game object when animation finished.
 public class PlanetClickAnimation : MonoBehaviour {
 
 	//global
 	private TextMesh _textMesh;
 	private Color _startColor;
-	private Color _endColor = Color.black;
+	private Color _endColor;
 	private float _timeElapsed;
 	
-	private float _animationTime = 1.4f;
+	private static float _animationColorTime = 1.4f;
+	
 	
 	// Use this for initialization
 	void Start ()
 	{
 		_textMesh = GetComponent<TextMesh>();
 		_startColor = _textMesh.color;
+		// end color is transparent start color
+		_endColor = new Color(_startColor.r, _startColor.g, _startColor.b, 0);
+		transform.localPosition = new Vector3(0.5f,0,0);
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
 		_timeElapsed += Time.deltaTime;
-		_textMesh.color = Color.Lerp(_startColor, _endColor, _timeElapsed / _animationTime);
-		if (_timeElapsed > _animationTime)
+		
+		// make y pos movement accelerate 
+		var animationMovementScale = _timeElapsed / (_animationColorTime * 20);
+		transform.localPosition += new Vector3(0, animationMovementScale, 0);
+		
+		// lerp color from opaque to transparent
+		_textMesh.color = Color.Lerp(_startColor, _endColor, _timeElapsed / _animationColorTime);
+		
+		// destroy when animation finished
+		if (_timeElapsed > _animationColorTime)
 		{
 			Destroy(gameObject);
 		}
